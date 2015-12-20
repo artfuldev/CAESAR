9ac31d819a785ddfab7bcbe07983c89d97249fae
Kenshin The Battōsai <kenshin@thebattosai.in>
2015-12-20 23:26:18 +0530

    Beginning to add unit tests. Also cleaning up target frameworks.

--- CAESAR/CAESAR.Chess.Tests/BoardTests.cs
+++ CAESAR/CAESAR.Chess.Tests/BoardTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CAESAR.Chess.Implementation;
+using System.Linq;
 using Xunit;
 
 namespace CAESAR.Chess.Tests
@@ -11,15 +9,69 @@ namespace CAESAR.Chess.Tests
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class BoardTests
     {
-        [Fact]
-        public void PassingTests()
+        private readonly IBoard _board;
+
+        public BoardTests() : this(new Board())
         {
-            var board = new Board();
-            var outputString = "";
-            foreach (var square in board.Squares)
-                outputString += square.Name + ", ";
-            outputString.TrimEnd(',');
-            Console.WriteLine(outputString);
+        }
+
+        // Injection Constructor
+        private BoardTests(IBoard board)
+        {
+            _board = board;
+        }
+
+
+        [Fact]
+        public void TotalNumberOfFilesInBoardIs8()
+        {
+            Assert.True(_board.Files.Count == 8);
+        }
+
+        [Theory]
+        [InlineData(1, 'a')]
+        [InlineData(2, 'b')]
+        [InlineData(3, 'c')]
+        [InlineData(4, 'd')]
+        [InlineData(5, 'e')]
+        [InlineData(6, 'f')]
+        [InlineData(7, 'g')]
+        [InlineData(8, 'h')]
+        public void NthFileInBoardIsNamedX(int n, char x)
+        {
+            Assert.True(_board.Files.ElementAt(n-1).Name == x);
+        }
+
+        [Fact]
+        public void TotalNumberOfRanksInBoardIs8()
+        {
+            Assert.True(_board.Ranks.Count == 8);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public void NthRankInBoardIsNumberedN(int n)
+        {
+            Assert.True(_board.Ranks.ElementAt(n - 1).Number == n);
+        }
+
+        [Fact]
+        public void TotalNumberOfSquaresInBoardIs64()
+        {
+            Assert.True(_board.Squares.Count == 64);
+        }
+
+        [Fact]
+        public void FirstSquareofBoardIsA1()
+        {
+            Assert.True(_board.Squares.ElementAt(0).Name == "a1");
         }
     }
 }
