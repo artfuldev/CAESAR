8d5a35056b24293937b2202e865c65776981010a
Kenshin The Battōsai <kenshin@thebattosai.in>
2015-12-25 12:00:09 +0530

    Added Backing Fields for FIle, Rank and Name

--- CAESAR/CAESAR.Chess/Implementation/Square.cs
+++ CAESAR/CAESAR.Chess/Implementation/Square.cs
@@ -13,9 +13,12 @@ namespace CAESAR.Chess.Implementation
             IsLight = isLight;
         }
         public IBoard Board { get; }
-        public IFile File => Board.Files.FirstOrDefault(x => x.Contains(this));
-        public IRank Rank => Board.Ranks.FirstOrDefault(x => x.Contains(this));
-        public string Name => (File.Name + Rank.Number.ToString()).ToLowerInvariant();
+        private IFile _file;
+        private IRank _rank;
+        private string _name;
+        public IFile File => _file ?? (_file = Board.Files.FirstOrDefault(x => x.Contains(this)));
+        public IRank Rank => _rank?? (_rank = Board.Ranks.FirstOrDefault(x => x.Contains(this)));
+        public string Name => _name ?? (_name = (File.Name + Rank.Number.ToString()));
         public bool IsLight { get; }
         public bool IsDark => !IsLight;
 
