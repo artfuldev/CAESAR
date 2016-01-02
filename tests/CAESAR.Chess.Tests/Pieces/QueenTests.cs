ed51bf0b175c38e78d3fd344d534ee4a38fadf0c
Kenshin The Battōsai <kenshin@thebattosai.in>
2016-01-03 00:41:01 +0530

    Added Queen Tests

---
+++ tests/CAESAR.Chess.Tests/Pieces/QueenTests.cs
@@ -0,0 +1,48 @@
+﻿using CAESAR.Chess.Implementation;
+using CAESAR.Chess.Pieces;
+using Xunit;
+
+namespace CAESAR.Chess.Tests.Pieces
+{
+    public class QueenTests
+    {
+        private readonly IPiece _whitePiece = new Queen(true);
+        private readonly IPiece _blackPiece = new Queen(false);
+        private readonly IBoard _board = new Board();
+        private readonly IPlayer _player = new Player();
+
+        [Fact]
+        public void NameOfQueenPieceIsQueen()
+        {
+            Assert.Equal("Queen",_whitePiece.Name);
+            Assert.Equal("Queen", _blackPiece.Name);
+        }
+
+        [Fact]
+        public void IsWhiteOfQueenPieceIsStored()
+        {
+            Assert.Equal(true, _whitePiece.IsWhite);
+            Assert.Equal(false, _blackPiece.IsWhite);
+        }
+
+        [Fact]
+        public void IsWhiteOfQueenPieceIsOppositeOfIsBlack()
+        {
+            Assert.NotEqual(_whitePiece.IsBlack, _whitePiece.IsWhite);
+            Assert.NotEqual(_blackPiece.IsBlack, _blackPiece.IsWhite);
+        }
+
+        [Fact]
+        public void NotationOfWhiteQueenIsQ()
+        {
+            Assert.Equal('Q', _whitePiece.Notation);
+        }
+
+        [Fact]
+        public void NotationOfBlackQueenIsq()
+        {
+            Assert.Equal('q', _blackPiece.Notation);
+        }
+
+    }
+}
