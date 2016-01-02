38a12d95d1d546a0515018345215f75a1ba84b7b
Kenshin The Battōsai <kenshin@thebattosai.in>
2016-01-03 00:42:27 +0530

    Added Bishop Tests

---
+++ tests/CAESAR.Chess.Tests/Pieces/BishopTests.cs
@@ -0,0 +1,48 @@
+﻿using CAESAR.Chess.Implementation;
+using CAESAR.Chess.Pieces;
+using Xunit;
+
+namespace CAESAR.Chess.Tests.Pieces
+{
+    public class BishopTests
+    {
+        private readonly IPiece _whitePiece = new Bishop(true);
+        private readonly IPiece _blackPiece = new Bishop(false);
+        private readonly IBoard _board = new Board();
+        private readonly IPlayer _player = new Player();
+
+        [Fact]
+        public void NameOfBishopPieceIsBishop()
+        {
+            Assert.Equal("Bishop",_whitePiece.Name);
+            Assert.Equal("Bishop", _blackPiece.Name);
+        }
+
+        [Fact]
+        public void IsWhiteOfBishopPieceIsStored()
+        {
+            Assert.Equal(true, _whitePiece.IsWhite);
+            Assert.Equal(false, _blackPiece.IsWhite);
+        }
+
+        [Fact]
+        public void IsWhiteOfBishopPieceIsOppositeOfIsBlack()
+        {
+            Assert.NotEqual(_whitePiece.IsBlack, _whitePiece.IsWhite);
+            Assert.NotEqual(_blackPiece.IsBlack, _blackPiece.IsWhite);
+        }
+
+        [Fact]
+        public void NotationOfWhiteBishopIsB()
+        {
+            Assert.Equal('B', _whitePiece.Notation);
+        }
+
+        [Fact]
+        public void NotationOfBlackBishopIsb()
+        {
+            Assert.Equal('b', _blackPiece.Notation);
+        }
+
+    }
+}
