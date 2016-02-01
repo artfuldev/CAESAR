4f8bd0fecf404103fdbbb4040369042a8e695871
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-01 21:28:10 +0530

    Added Pawn Promotion

--- src/CAESAR.Chess/Moves/Move.cs
+++ src/CAESAR.Chess/Moves/Move.cs
@@ -9,24 +9,18 @@ namespace CAESAR.Chess.Moves
     public class Move : IMove
     {
         private static readonly INotation DefaultNotation = new PureCoordinateNotation();
-        public Move(IPiece piece, ISquare destination, MoveType moveType = MoveType.Normal, IPiece promotionPiece = null)
+        public Move(IPiece piece, ISquare destination, IPiece promotionPiece = null)
         {
             Piece = piece;
             Destination = destination;
-            MoveType = moveType;
             Source = piece.Square;
-            Side = piece.Side;
-            if (moveType == MoveType.Promotion)
-                PromotionPiece = promotionPiece;
-            if (moveType == MoveType.Capture)
-                CapturedPiece = Destination.Piece;
+            PromotionPiece = promotionPiece;
+            CapturedPiece = Destination.Piece;
         }
 
         public ISquare Source { get; }
         public ISquare Destination { get; }
         public IPiece Piece { get; }
-        public Side Side { get; }
-        public MoveType MoveType { get; }
         public IPiece PromotionPiece { get; }
         public IPiece CapturedPiece { get; }
 
