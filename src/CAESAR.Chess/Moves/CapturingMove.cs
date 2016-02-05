cf869b05dffd5f11eb42b2e1187da7927999bbec
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 22:02:27 +0530

    Added "rook captures disable castling" rule

--- src/CAESAR.Chess/Moves/CapturingMove.cs
+++ src/CAESAR.Chess/Moves/CapturingMove.cs
@@ -1,4 +1,6 @@
-﻿using CAESAR.Chess.Core;
+﻿using System.Linq;
+using CAESAR.Chess.Core;
+using CAESAR.Chess.Pieces;
 using CAESAR.Chess.PlayArea;
 using CAESAR.Chess.Positions;
 
@@ -14,6 +16,27 @@ namespace CAESAR.Chess.Moves
         {
             position = base.MakeImplementation(position);
             position.HalfMoveClock = 0;
+            var square = Source.Board.GetSquare(DestinationSquareName);
+            var destinationRank = Side == Side.White ? 1 : 8;
+            var destinationSquares = new [] {"a"+destinationRank,"h"+destinationRank};
+            var destinationSquare = Source.Board.GetSquare(DestinationSquareName);
+            if (square.Piece.PieceType == PieceType.Rook && destinationSquares.Contains(DestinationSquareName))
+            {
+                if (Side == Side.White)
+                {
+                    if (destinationSquare.File.Name == 'a')
+                        position.CastlingRights &= ~CastlingRights.WhiteLong;
+                    if (destinationSquare.File.Name == 'h')
+                        position.CastlingRights &= ~CastlingRights.WhiteShort;
+                }
+                if (Side == Side.Black)
+                {
+                    if (destinationSquare.File.Name == 'a')
+                        position.CastlingRights &= ~CastlingRights.BlackLong;
+                    if (destinationSquare.File.Name == 'h')
+                        position.CastlingRights &= ~CastlingRights.BlackShort;
+                }
+            }
             return position;
         }
     }
