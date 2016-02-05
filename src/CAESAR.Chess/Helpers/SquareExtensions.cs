582d5d61364b02f870cbb9de57f22c8f0f24a3e5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 22:48:47 +0530

    Added comments for SquareExtensions

--- src/CAESAR.Chess/Helpers/SquareExtensions.cs
+++ src/CAESAR.Chess/Helpers/SquareExtensions.cs
@@ -5,13 +5,36 @@ using CAESAR.Chess.PlayArea;
 
 namespace CAESAR.Chess.Helpers
 {
+    /// <summary>
+    ///     Useful extensions for an <seealso cref="ISquare" />.
+    /// </summary>
     public static class SquareExtensions
     {
+        /// <summary>
+        ///     Gets a single <seealso cref="ISquare" /> adjacent to a particular <seealso cref="ISquare" /> in a particular
+        ///     <seealso cref="Direction" />.
+        /// </summary>
+        /// <param name="square">The <seealso cref="ISquare" /> around which to look for the adjacent <seealso cref="ISquare" />.</param>
+        /// <param name="direction">The <seealso cref="Direction" /> in which to look for the adjacent <seealso cref="ISquare" />.</param>
+        /// <returns>
+        ///     The <seealso cref="ISquare" /> adjacent to the <seealso cref="square" /> in the specified
+        ///     <seealso cref="Direction" /> if available, null otherwise.
+        /// </returns>
         public static ISquare GetAdjacentSquareInDirection(this ISquare square, Direction direction)
         {
             return square.GetAdjacentSquaresInDirection(direction).FirstOrDefault();
         }
 
+        /// <summary>
+        ///     Gets an <seealso cref="IEnumerable{ISquare}" /> adjacent to a particular <seealso cref="ISquare" /> with in a
+        ///     particular <seealso cref="Direction" />.
+        /// </summary>
+        /// <param name="square">The <seealso cref="ISquare" /> around which to look for the adjacent <seealso cref="ISquare" />s.</param>
+        /// <param name="direction">The <seealso cref="Direction" /> in which to look for the adjacent <seealso cref="ISquare" />s.</param>
+        /// <returns>
+        ///     The <seealso cref="ISquare" />s adjacent to the <seealso cref="square" /> in the specified
+        ///     <seealso cref="Direction" /> if available, as an <seealso cref="IEnumerable{ISquare}" />.
+        /// </returns>
         public static IEnumerable<ISquare> GetAdjacentSquaresInDirection(this ISquare square, Direction direction)
         {
             if (ReferenceEquals(null, square) || direction == Direction.None)
