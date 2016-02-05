cf84270ec058e4737b9bc4a72f6bbada3a8741a6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 22:13:55 +0530

    Added comments for Direction

--- src/CAESAR.Chess/Core/Direction.cs
+++ src/CAESAR.Chess/Core/Direction.cs
@@ -1,18 +1,53 @@
 ﻿namespace CAESAR.Chess.Core
 {
     /// <summary>
-    /// A direction of navigation/reference for the board
+    ///     A direction of navigation/reference for the board
     /// </summary>
     public enum Direction : byte
     {
+        /// <summary>
+        ///     No direction.
+        /// </summary>
         None,
+
+        /// <summary>
+        ///     The upward direction.
+        /// </summary>
         Up,
+
+        /// <summary>
+        ///     The right direction.
+        /// </summary>
         Right,
+
+        /// <summary>
+        ///     The downward direction.
+        /// </summary>
         Down,
+
+        /// <summary>
+        ///     The left direction.
+        /// </summary>
         Left,
+
+        /// <summary>
+        ///     The up-right direction.
+        /// </summary>
         UpRight,
+
+        /// <summary>
+        ///     The down-right direction.
+        /// </summary>
         DownRight,
+
+        /// <summary>
+        ///     The down-left direction.
+        /// </summary>
         DownLeft,
+
+        /// <summary>
+        ///     The up-left direction.
+        /// </summary>
         UpLeft
     }
 }
