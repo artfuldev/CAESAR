519d652b2221eef2de8deb26270b7c2ae8faec2c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 19:13:54 +0530

    Add Castling Move

---
+++ src/CAESAR.Chess/Moves/CastlingMove.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CAESAR.Chess.PlayArea;
+
+namespace CAESAR.Chess.Moves
+{
+    public class CastlingMove : NormalMove
+    {
+        public CastlingMove(ISquare source, CastlingType castlingType)
+            : base(source, GetDestinationSquareName(castlingType))
+        {
+        }
+
+        static string GetDestinationSquareName(CastlingType type)
+        {
+            switch (type)
+            {
+                case CastlingType.WhiteShort:
+                    return "g1";
+                case CastlingType.WhiteLong:
+                    return "c1";
+                case CastlingType.BlackShort:
+                    return "g8";
+                case CastlingType.BlackLong:
+                    return "c8";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, "Invalid castling type");
+            }
+        }
+    }
+
+    public enum CastlingType
+    {
+        WhiteShort,
+        WhiteLong,
+        BlackShort,
+        BlackLong
+    }
+}
