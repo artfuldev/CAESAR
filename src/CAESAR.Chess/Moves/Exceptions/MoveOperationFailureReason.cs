d5a9cf3ef9796873fdaab5cda85de42efade7c47
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 23:00:08 +0530

    Added comments to MoveOperationFailureReason

--- src/CAESAR.Chess/Moves/Exceptions/MoveOperationFailureReason.cs
+++ src/CAESAR.Chess/Moves/Exceptions/MoveOperationFailureReason.cs
@@ -1,9 +1,27 @@
+using CAESAR.Chess.Core;
+using CAESAR.Chess.Players;
+
 namespace CAESAR.Chess.Moves.Exceptions
 {
+    /// <summary>
+    ///     The reason for failure of the move operation.
+    /// </summary>
     public enum MoveOperationFailureReason
     {
+        /// <summary>
+        ///     Unknown reason.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        ///     The source square of the <seealso cref="IMove" /> is empty.
+        /// </summary>
         SourceSquareIsEmpty,
+
+        /// <summary>
+        ///     The <seealso cref="IPlayer" /> used to play the <seealso cref="IMove" /> belongs to a different
+        ///     <seealso cref="Side" />.
+        /// </summary>
         PlayerNotOnCorrectSide
     }
 }
