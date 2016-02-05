b7f0ed3527cdc7aca29939e84d08120dcb2ee6c5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-05 22:41:44 +0530

    Added comments to Side Extensions

--- src/CAESAR.Chess/Helpers/SideExtensions.cs
+++ src/CAESAR.Chess/Helpers/SideExtensions.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using CAESAR.Chess.Core;
+using CAESAR.Chess.Pieces;
 
 namespace CAESAR.Chess.Helpers
 {
+    /// <summary>
+    ///     Provides useful extensions to the <seealso cref="Side" /> enumeration.
+    /// </summary>
     public static class SideExtensions
     {
+        /// <summary>
+        ///     Converts a <seealso cref="char" /> denoting a side to a <seealso cref="Side" />.
+        /// </summary>
+        /// <param name="side">The <seealso cref="char" /> denoting the <seealso cref="Side" />.</param>
+        /// <returns>The <seealso cref="Side" /> denoted by the <seealso cref="char" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     When the <seealso cref="side" /> is not <seealso cref="Side.Black" /> or
+        ///     <seealso cref="Side.White" />.
+        /// </exception>
         public static Side ToSide(this char side)
         {
             switch (side)
@@ -20,6 +33,15 @@ namespace CAESAR.Chess.Helpers
             }
         }
 
+        /// <summary>
+        ///     Converts a <seealso cref="Side" /> to its Active Color <seealso cref="char" /> representation.
+        /// </summary>
+        /// <param name="side">The <seealso cref="Side" /> to convert to Active Color.</param>
+        /// <returns>The active color <seealso cref="char" /> representation of the <seealso cref="side" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     When the <seealso cref="side" /> is not <seealso cref="Side.Black" /> or
+        ///     <seealso cref="Side.White" />.
+        /// </exception>
         public static char GetActiveColor(this Side side)
         {
             switch (side)
@@ -33,6 +55,15 @@ namespace CAESAR.Chess.Helpers
             }
         }
 
+        /// <summary>
+        ///     Gets the <seealso cref="Side" /> of the <seealso cref="IPiece" /> represented by the <seealso cref="char" />.
+        /// </summary>
+        /// <param name="notation">The <seealso cref="char" /> notation of an <seealso cref="IPiece" />.</param>
+        /// <returns>The <seealso cref="Side" /> of the <seealso cref="IPiece" /> represented by the <seealso cref="notation" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     When the <seealso cref="notation" /> does not belong to any recognized
+        ///     <seealso cref="IPiece" />.
+        /// </exception>
         public static Side GetPieceSide(this char notation)
         {
             switch (notation)
