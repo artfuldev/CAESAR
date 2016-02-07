81fe3e363d5cd0fa8b22f8d70fb1fd0633af82bb
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-07 11:52:31 +0530

    Added comments

--- src/CAESAR.Chess/Moves/Notations/PureCoordinateNotation.cs
+++ src/CAESAR.Chess/Moves/Notations/PureCoordinateNotation.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using CAESAR.Chess.Pieces;
-using CAESAR.Chess.PlayArea;
+﻿using CAESAR.Chess.Pieces;
 
 namespace CAESAR.Chess.Moves.Notations
 {
@@ -9,10 +7,15 @@ namespace CAESAR.Chess.Moves.Notations
     ///     This notation omits any machine redundant piece letters for the moving and/or capturing pieces, and only has to
     ///     specify the promoted piece as trailing letter in case of promotions.
     /// </summary>
-    /// <example>e2e4, f7f8q</example>
+    /// <example>e2e4, f7f8Q</example>
     /// <remarks>https://chessprogramming.wikispaces.com/Algebraic+Chess+Notation#Pure%20coordinate%20notation</remarks>
     public class PureCoordinateNotation : INotation
     {
+        /// <summary>
+        ///     Gets the string representation of the <seealso cref="IMove" /> in <seealso cref="PureCoordinateNotation" />.
+        /// </summary>
+        /// <param name="move">The <seealso cref="IMove" /> for which the notation is required.</param>
+        /// <returns>The string representation of the <seealso cref="IMove" /> in <seealso cref="PureCoordinateNotation" />.</returns>
         public string ToString(IMove move)
         {
             var normalMove = move as NormalMove;
