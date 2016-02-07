f08fd5ea73966b1de11e5c74af772b074cab1fce
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-07 11:46:57 +0530

    Added comments

--- src/CAESAR.Chess/Moves/Generation/QueenMovesGenerator.cs
+++ src/CAESAR.Chess/Moves/Generation/QueenMovesGenerator.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
 using CAESAR.Chess.Core;
 using CAESAR.Chess.Helpers;
+using CAESAR.Chess.Pieces;
+using CAESAR.Chess.PlayArea;
 
 namespace CAESAR.Chess.Moves.Generation
 {
+    /// <summary>
+    ///     Generates <seealso cref="IMove" />s for a particular <seealso cref="ISquare" />, based on the move
+    ///     generation rules of the <seealso cref="Queen" />.
+    /// </summary>
     public class QueenMovesGenerator : DirectedMovesGenerator
     {
+        /// <summary>
+        ///     The <seealso cref="Direction" />s that a <seealso cref="Queen" /> is allowed to move in.
+        /// </summary>
         private static readonly IEnumerable<Direction> AllowedDirections = new[]
         {
             Direction.Up,
@@ -18,6 +27,10 @@ namespace CAESAR.Chess.Moves.Generation
             Direction.UpLeft
         };
 
+        /// <summary>
+        ///     The <seealso cref="Direction" />s in which this <seealso cref="MovesGenerator" /> can generate directed
+        ///     <seealso cref="IMove" />s.
+        /// </summary>
         protected override IEnumerable<Direction> Directions => AllowedDirections;
     }
 }
