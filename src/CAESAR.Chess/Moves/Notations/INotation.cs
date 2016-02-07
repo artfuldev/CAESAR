1aff04985a7cf4a5cbfa0ddaded6da45009d2aa1
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-07 11:50:18 +0530

    Added comments

--- src/CAESAR.Chess/Moves/Notations/INotation.cs
+++ src/CAESAR.Chess/Moves/Notations/INotation.cs
@@ -1,12 +1,19 @@
-﻿using CAESAR.Chess.PlayArea;
+﻿using CAESAR.Chess.Games;
+using CAESAR.Chess.Positions;
 
 namespace CAESAR.Chess.Moves.Notations
 {
     /// <summary>
-    ///     Denotes a system to record either the moves made in a game of chess or the position of pieces on a chessboard.
+    ///     Denotes a system to record either the <seealso cref="IMove" />s made in an <seealso cref="IGame" /> of chess, or
+    ///     the <seealso cref="IPosition" /> of a chessboard.
     /// </summary>
     public interface INotation
     {
+        /// <summary>
+        ///     Gets the string representation of the <seealso cref="IMove" /> in this particular <seealso cref="INotation" />.
+        /// </summary>
+        /// <param name="move">The <seealso cref="IMove" /> for which the notation is required.</param>
+        /// <returns>The string representation of the <seealso cref="IMove" /> in this particular <seealso cref="INotation" />.</returns>
         string ToString(IMove move);
     }
 }
