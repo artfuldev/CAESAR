7ed74be7e5e2c4db23e3bab2ed556ef5957df8ae
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-09 22:18:20 +0530

    Added comments

--- src/CAESAR.Chess/Pieces/Queen.cs
+++ src/CAESAR.Chess/Pieces/Queen.cs
@@ -1,10 +1,21 @@
 ﻿using CAESAR.Chess.Core;
+using CAESAR.Chess.Games;
+using CAESAR.Chess.Moves;
 using CAESAR.Chess.Moves.Generation;
+using CAESAR.Chess.PlayArea;
 
 namespace CAESAR.Chess.Pieces
 {
+    /// <summary>
+    ///     Represents a queen in the <seealso cref="IGame" />. It belongs to a <seealso cref="Side" />, occupies an
+    ///     <seealso cref="ISquare" />, and can have <seealso cref="IMove" />s available.
+    /// </summary>
     public class Queen : Piece
     {
+        /// <summary>
+        ///     Instantiates a <seealso cref="Queen" /> of a particular <seealso cref="Side" />.
+        /// </summary>
+        /// <param name="side">The <seealso cref="Side" /> to which this <seealso cref="Queen" /> belongs.</param>
         public Queen(Side side) : base(side, PieceType.Queen, new QueenMovesGenerator())
         {
         }
