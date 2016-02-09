3358d970ba528f6ba7dad802c953dafa3de3089f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-09 22:27:14 +0530

    Added comments

--- src/CAESAR.Chess/Pieces/PieceTypeExtensions.cs
+++ src/CAESAR.Chess/Pieces/PieceTypeExtensions.cs
@@ -4,8 +4,20 @@ using CAESAR.Chess.Helpers;
 
 namespace CAESAR.Chess.Pieces
 {
+    /// <summary>
+    ///     Provides useful extensions to the <seealso cref="PieceType" /> enumeration.
+    /// </summary>
     public static class PieceTypeExtensions
     {
+        /// <summary>
+        ///     Gets the notation corresponding to a particular <seealso cref="PieceType" />.
+        /// </summary>
+        /// <param name="pieceType">The <seealso cref="PieceType" /> for which to get the notation.</param>
+        /// <returns>The notation corresponding to a particular <seealso cref="pieceType" /> as a <seealso cref="char" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the specififed <seealso cref="pieceType" /> does not have a
+        ///     matching notation.
+        /// </exception>
         public static char GetNotation(this PieceType pieceType)
         {
             switch (pieceType)
@@ -27,6 +39,15 @@ namespace CAESAR.Chess.Pieces
             }
         }
 
+        /// <summary>
+        ///     Gets the <seealso cref="PieceType" /> corresponding to a <seealso cref="char" /> notation.
+        /// </summary>
+        /// <param name="notation">The notation of the piece as a <seealso cref="char" />.</param>
+        /// <returns>The <seealso cref="PieceType" /> corresponding to its <seealso cref="notation" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the specififed <seealso cref="notation" /> does not have a
+        ///     matching <seealso cref="PieceType" />.
+        /// </exception>
         public static PieceType GetPieceType(this char notation)
         {
             switch (notation)
@@ -54,6 +75,20 @@ namespace CAESAR.Chess.Pieces
             }
         }
 
+        /// <summary>
+        ///     Gets a new instance of an <seealso cref="IPiece" /> of a particular <seealso cref="PieceType" /> belonging to a
+        ///     given <seealso cref="Side" />.
+        /// </summary>
+        /// <param name="type">The <seealso cref="PieceType" /> of the new <seealso cref="IPiece" /> instance.</param>
+        /// <param name="side">The <seealso cref="Side" /> to which the new <seealso cref="IPiece" /> instance must belong.</param>
+        /// <returns>
+        ///     A new instance of an <seealso cref="IPiece" /> of a particular <seealso cref="type" /> belonging to a given
+        ///     <seealso cref="side" />.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the specififed <seealso cref="type" /> does not have a
+        ///     matching <seealso cref="IPiece" /> instance.
+        /// </exception>
         public static IPiece GetPiece(this PieceType type, Side side)
         {
             switch (type)
@@ -75,8 +110,11 @@ namespace CAESAR.Chess.Pieces
             }
         }
 
-        
-
+        /// <summary>
+        ///     Gets a new instance of an <seealso cref="IPiece" /> corresponding to its <seealso cref="char" /> notation.
+        /// </summary>
+        /// <param name="notation">The notaiton of the <seealso cref="IPiece" /> as a <seealso cref="char" />.</param>
+        /// <returns>A new instance of an <seealso cref="IPiece" /> corresponding to its <seealso cref="notation" />.</returns>
         public static IPiece GetPiece(this char notation)
         {
             return GetPiece(notation.GetPieceType(), notation.GetPieceSide());
