573c26772b9809aa81ae5f231ed58e126fdb9079
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-10 21:52:12 +0530

    Added comments

--- src/CAESAR.Chess/PlayArea/IFile.cs
+++ src/CAESAR.Chess/PlayArea/IFile.cs
@@ -2,10 +2,24 @@
 
 namespace CAESAR.Chess.PlayArea
 {
+    /// <summary>
+    ///     Represents a file on the <seealso cref="IBoard" />. It's a named column on the chessboard.
+    /// </summary>
     public interface IFile
     {
+        /// <summary>
+        ///     The name of this <seealso cref="IFile" />
+        /// </summary>
         char Name { get; }
+
+        /// <summary>
+        ///     The <seealso cref="IBoard" /> to which this <seealso cref="IFile" /> belongs.
+        /// </summary>
         IBoard Board { get; }
+
+        /// <summary>
+        ///     The <seealso cref="ISquare" />s that make up this <seealso cref="IFile" />.
+        /// </summary>
         IReadOnlyCollection<ISquare> Squares { get; }
     }
 }
