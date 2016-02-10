1b25a94b75a2fc450a804606856a7286eccad088
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-10 22:10:27 +0530

    Added comments

--- src/CAESAR.Chess/PlayArea/Square.cs
+++ src/CAESAR.Chess/PlayArea/Square.cs
@@ -3,10 +3,27 @@ using CAESAR.Chess.Pieces;
 
 namespace CAESAR.Chess.PlayArea
 {
+    /// <summary>
+    ///     Represents a square on the <seealso cref="IBoard" />. It's a named square on the chessboard, belonging to a
+    ///     <seealso cref="IFile" /> and an <seealso cref="IRank" />. It can be light or dark, and can be empty or can be
+    ///     occupied by an <seealso cref="IPiece" />.
+    /// </summary>
     public class Square : ISquare
     {
+        /// <summary>
+        ///     The backing field for <seealso cref="Piece" />.
+        /// </summary>
         private IPiece _piece;
 
+        /// <summary>
+        ///     Instantiates a <seealso cref="Square" />, with an <seealso cref="IBoard" />, an <seealso cref="IFile" />, an
+        ///     <seealso cref="IRank" />, a <seealso cref="name" />, and a boolean indicating whether it is light.
+        /// </summary>
+        /// <param name="board">The <seealso cref="IBoard" /> to which this <seealso cref="Square" /> belongs.</param>
+        /// <param name="file">The <seealso cref="IFile" /> to which this <seealso cref="Square" /> belongs.</param>
+        /// <param name="rank">The <seealso cref="IRank" /> to which this <seealso cref="Square" /> belongs.</param>
+        /// <param name="name">The name of this <seealso cref="Square" />.</param>
+        /// <param name="isLight">A boolean indicating whether this <seealso cref="Square" /> is light.</param>
         public Square(IBoard board, IFile file, IRank rank, string name, bool isLight = false)
         {
             if (board == null)
@@ -27,8 +44,16 @@ namespace CAESAR.Chess.PlayArea
             IsLight = isLight;
         }
 
+        /// <summary>
+        ///     The <seealso cref="IBoard" /> to which this <seealso cref="Square" /> belongs.
+        /// </summary>
         public IBoard Board { get; }
 
+        /// <summary>
+        ///     The <seealso cref="IPiece" /> that occupies this <seealso cref="Square" />, if any. Setting an
+        ///     <seealso cref="IPiece" /> also sets the <seealso cref="IPiece" />'s <seealso cref="ISquare" /> to this instance of
+        ///     <seealso cref="ISquare" />.
+        /// </summary>
         public IPiece Piece
         {
             get { return _piece; }
@@ -40,14 +65,47 @@ namespace CAESAR.Chess.PlayArea
             }
         }
 
+        /// <summary>
+        ///     The <seealso cref="IFile" /> to which this <seealso cref="Square" /> belongs.
+        /// </summary>
         public IFile File { get; }
+
+        /// <summary>
+        ///     The <seealso cref="IRank" /> to which this <seealso cref="Square" /> belongs.
+        /// </summary>
         public IRank Rank { get; }
+
+        /// <summary>
+        ///     The name of this <seealso cref="Square" />.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        ///     A boolean that tells if this <seealso cref="Square" /> is light.
+        /// </summary>
         public bool IsLight { get; }
+
+        /// <summary>
+        ///     A boolean that tells if this <seealso cref="Square" /> is dark.
+        /// </summary>
         public bool IsDark => !IsLight;
+
+        /// <summary>
+        ///     A boolean that tells if this <seealso cref="Square" /> is empty.
+        /// </summary>
         public bool IsEmpty => Piece == null;
+
+        /// <summary>
+        ///     A boolean that tells if this <seealso cref="Square" /> is occupied by an <seealso cref="IPiece" />.
+        /// </summary>
         public bool HasPiece => !IsEmpty;
 
+        /// <summary>
+        ///     Returns a string that represents the current <seealso cref="Square" />. This is usually the <seealso cref="Name" />
+        ///     of the <seealso cref="Square" />.
+        /// </summary>
+        /// <returns>The <seealso cref="Name" /> of the <seealso cref="Square" />.</returns>
+        /// <filterpriority>2</filterpriority>
         public override string ToString()
         {
             return Name;
