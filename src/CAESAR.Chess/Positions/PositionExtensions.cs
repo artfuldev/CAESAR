5b7632bde38a4df63c7912aee58989355113e525
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-11 22:05:04 +0530

    Added comments

--- src/CAESAR.Chess/Positions/PositionExtensions.cs
+++ src/CAESAR.Chess/Positions/PositionExtensions.cs
@@ -1,19 +1,31 @@
-﻿using CAESAR.Chess.Core;
+﻿using System.Linq;
+using CAESAR.Chess.Core;
 using CAESAR.Chess.Moves;
 using CAESAR.Chess.Pieces;
-using CAESAR.Chess.Players;
-using System.Linq;
 
 namespace CAESAR.Chess.Positions
 {
+    /// <summary>
+    ///     Useful extensions for an <seealso cref="IPosition" />.
+    /// </summary>
     public static class PositionExtensions
     {
+        /// <summary>
+        ///     Calculates whether a given <seealso cref="Side" /> is in check, in a given <seealso cref="IPosition" />.
+        /// </summary>
+        /// <param name="position">The <seealso cref="IPosition" /> in which to calculate.</param>
+        /// <param name="side">The <seealso cref="Side" /> for which to calculate.</param>
+        /// <returns>
+        ///     True if the given <seealso cref="side" /> is in check in the given <seealso cref="position" />, false
+        ///     otherwise.
+        /// </returns>
         public static bool IsInCheck(this IPosition position, Side side)
         {
             var opposingSide = side == Side.White ? Side.Black : Side.White;
             var opponentMoves =
                 position.Board.Squares.Where(
-                    square => square.HasPiece && square.Piece.Side == opposingSide && square.Piece.PieceType != PieceType.King)
+                    square =>
+                        square.HasPiece && square.Piece.Side == opposingSide && square.Piece.PieceType != PieceType.King)
                     .Select(square => square.Piece)
                     .SelectMany(piece => piece.Moves);
 
