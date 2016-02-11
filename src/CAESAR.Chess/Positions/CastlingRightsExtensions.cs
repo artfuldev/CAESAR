7fba759d5feb1675bf2481f8aac857a2653aedcd
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-11 21:40:26 +0530

    Added comments

--- src/CAESAR.Chess/Positions/CastlingRightsExtensions.cs
+++ src/CAESAR.Chess/Positions/CastlingRightsExtensions.cs
@@ -3,15 +3,29 @@ using System.Linq;
 
 namespace CAESAR.Chess.Positions
 {
+    /// <summary>
+    ///     Useful extensions for the <seealso cref="CastlingRights" /> enumeration.
+    /// </summary>
     public static class CastlingRightsExtensions
     {
+        /// <summary>
+        ///     Converts a <seealso cref="string" /> representing castling rights to <seealso cref="CastlingRights" />.
+        /// </summary>
+        /// <param name="castlingRights">The castling rights as a <seealso cref="string" />.</param>
+        /// <returns>The <seealso cref="CastlingRights" /> represented by the <seealso cref="string" />.</returns>
         public static CastlingRights ToCastlingRights(this string castlingRights)
         {
             if (string.IsNullOrWhiteSpace(castlingRights) || castlingRights == "-")
                 return CastlingRights.None;
-            return castlingRights.Aggregate(CastlingRights.None, (current, castlingRight) => current | castlingRight.ToCastlingRights());
+            return castlingRights.Aggregate(CastlingRights.None,
+                (current, castlingRight) => current | castlingRight.ToCastlingRights());
         }
 
+        /// <summary>
+        ///     Converts a <seealso cref="char" /> representing castling rights to <seealso cref="CastlingRights" />.
+        /// </summary>
+        /// <param name="castlingRights">The castling rights as a <seealso cref="char" />.</param>
+        /// <returns>The <seealso cref="CastlingRights" /> represented by the <seealso cref="char" />.</returns>
         private static CastlingRights ToCastlingRights(this char castlingRights)
         {
             switch (castlingRights)
@@ -31,6 +45,15 @@ namespace CAESAR.Chess.Positions
             }
         }
 
+        /// <summary>
+        ///     Converts a <seealso cref="CastlingRights" /> enumeration to its corresponding <seealso cref="string" />
+        ///     representation.
+        /// </summary>
+        /// <param name="castlingRights">
+        ///     The <seealso cref="CastlingRights" /> for which a  <seealso cref="string" />
+        ///     representation is needed.
+        /// </param>
+        /// <returns>The <seealso cref="string" /> representing the given <seealso cref="castlingRights" />.</returns>
         public static string ToCastlingAvailability(this CastlingRights castlingRights)
         {
             if (castlingRights == CastlingRights.None)
