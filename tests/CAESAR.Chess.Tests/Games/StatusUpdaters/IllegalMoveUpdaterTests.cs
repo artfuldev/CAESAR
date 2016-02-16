4fa11690e31e452d5153b9a2f62a5235c2609a1c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-16 21:49:18 +0530

    Adding IllegalMoveUpdater tests

---
+++ tests/CAESAR.Chess.Tests/Games/StatusUpdaters/IllegalMoveUpdaterTests.cs
@@ -0,0 +1,9 @@
+﻿using CAESAR.Chess.Games.Statuses.Updaters;
+
+namespace CAESAR.Chess.Tests.Games.StatusUpdaters
+{
+    public class IllegalMoveUpdaterTests
+    {
+        private readonly IStatusUpdater _updater = new IllegalMoveUpdater();
+    }
+}
