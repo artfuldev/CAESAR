efaf1bda06cffed77db3e675a0d26ad2f05c0b92
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-27 21:28:56 +0530

    Added Piece Placement Test

---
+++ tests/CAESAR.Chess.Tests/Positions/PositionTests.cs
@@ -0,0 +1,24 @@
+﻿using CAESAR.Chess.Positions;
+using Xunit;
+
+namespace CAESAR.Chess.Tests.Positions
+{
+    public class PositionTests
+    {
+        private readonly IPosition _position = new Position();
+
+        [Theory]
+        [InlineData("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")]
+        public void FenStringSetsPiecePlacementOnPosition(string fen, string piecePlacement)
+        {
+            // Arrange
+
+            // Act
+            _position.SetPosition(fen);
+            var actual = _position.ToFenString().PiecePlacement;
+
+            // Assert
+            Assert.Equal(piecePlacement, actual);
+        } 
+    }
+}
