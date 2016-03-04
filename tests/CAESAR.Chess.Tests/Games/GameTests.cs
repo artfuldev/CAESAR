6ec39c5c8eec35b5ff4d384983a7d97b9b83482e
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-04 22:43:52 +0530

    Added test to know if IPlayer IGame.White's side is Side.White

--- tests/CAESAR.Chess.Tests/Games/GameTests.cs
+++ tests/CAESAR.Chess.Tests/Games/GameTests.cs
@@ -1,9 +1,17 @@
 ﻿using CAESAR.Chess.Games;
+using CAESAR.Chess.Core;
+using Xunit;
 
 namespace CAESAR.Chess.Tests.Games
 {
     public class GameTests
     {
         private readonly IGame _game = new Game();
+
+        [Fact]
+        public void GamesWhitePlayersSideIsWhite()
+        {
+            Assert.Equal(Side.White, _game.White.Side);
+        }
     }
 }
